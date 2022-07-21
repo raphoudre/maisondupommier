4152785cdccacdbdcf51827144418b1c9159cd3c
Raphoudre <raphael.ivol@hotmail.fr>
2022-07-21 16:10:15 +0200

    c chaud

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route exact path='/' element={<Home/>}/>
-        <Route exact path='/about' element={<About/>}/>
+        <Route path='about' element={<About/>}/>
       </Routes>
     </Router>
   );
